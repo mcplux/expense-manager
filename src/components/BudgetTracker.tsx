d489103d2642fd23759d8e1312ee9f3f25f66733
Juan Martinez <pablomar01@outlook.com>
2025-02-10 08:24:56 -0500

    add graph

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,13 +1,30 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import { useBudget } from '../hooks/useBudget'
 import AmountDisplay from './AmountDisplay'
+
+import 'react-circular-progressbar/dist/styles.css'
 
 function BudgetTracker() {
   const { state, remainingBudget, totalExpenses } = useBudget()
 
+  const percentageRemainingBudget = +(
+    (remainingBudget * 100) /
+    state.budget
+  ).toFixed(2)
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
-        <img src="/graph.jpg" alt="Chart" />
+        <CircularProgressbar
+          value={percentageRemainingBudget}
+          styles={buildStyles({
+            pathColor: percentageRemainingBudget < 10 ? '#D62626' : '#3B82F6',
+            trailColor: '#F5F5F5',
+            textSize: 8,
+            textColor: percentageRemainingBudget < 10 ? '#D62626' : '#3B82F6',
+          })}
+          text={`${percentageRemainingBudget}% Remaining`}
+        />
       </div>
       <div className="flex flex-col justify-center items-center gap-8">
         <button
